5f5053e8fd07105f9bd431e9a69a3ba6141b31bc
Ekramvis <ekramvis@gmail.com>
2013-05-10 16:24:35 -0400

    Teams and Users can add each other

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,51 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+
+		render :new
+	end
+
+	def create
+		@user = User.new(params[:user])
+
+		if @user.save
+			flash.notice = "User Created"
+			redirect_to users_url
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+
+		render :edit
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(params[:user])
+
+		if @user.save
+			flash.notice = "User Saved"
+			redirect_to users_url
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id]).destroy
+
+		redirect_to users_url 
+	end 
 
 end
